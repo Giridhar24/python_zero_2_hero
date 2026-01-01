3cb0cdf0716bf297b4543e63b3c11e17cba1a8da
giridhar24 <vasugiridhar@yahoo.com>
2026-01-01 22:19:29 +0530

    All Local Changes

--- 1-Python Overview/gpaCalculator.py
+++ 1-Python Overview/gpaCalculator.py
@@ -2,7 +2,7 @@ print( 'Welcome to the GPA calculator.' )
 print( 'Please enter all your letter grades, one per line.' )
 print( 'Enter a blank line to designate the end.' )
 
-# print( """
+# print("""
 # Welcome to the GPA calculator.
 # Please enter all your letter grades, one per line.
 # Enter a blank line to designate the end.
